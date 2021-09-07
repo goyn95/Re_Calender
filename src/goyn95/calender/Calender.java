95d050985eedea925d149aa056c94809e149492b
goyn95 <goyn9501@gmail.com>
2021-09-07 22:31:29 +0900

    creat class prompt & days

--- src/goyn95/calender/Calender.java
+++ src/goyn95/calender/Calender.java
@@ -11,38 +11,27 @@ public class Calender {
 
 	}
 
-	public void printSampleCalender() {
-		System.out.println(" 일 월  화  수 목 금  토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-
-	public static void main(String[] args) {
-
-		// 숫자로 입력받아 해당하는달의 최대일수를 출력하는 프로그램
-		String PROMPT = "cal> ";
-		Scanner scanner = new Scanner(System.in);
-		Calender cal = new Calender();
-
-
-		int month = 1;
-		while (true) {
-			System.out.println("달을 입력하세요.");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if (month == -1) {
-				break;
+	public void printCalender(int year, int month) {
+		System.out.printf("    <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		
+		int maxDay = getmaxDaysOfMonth(month);
+		
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d",i);
+			if(i % 7 == 0) {
+				System.out.println();
 			}
-			if (month > 12) {
-				continue;
-			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
 		}
+		
+		System.out.println();
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+//		System.out.println("29 30 31");
 
-		System.out.println("fin~");
-		scanner.close();
 	}
+
 }
---
+++ src/goyn95/calender/Prompt.java
@@ -0,0 +1,40 @@
+package goyn95.calender;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal> ";
+
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
+
+		int month = 1;
+		while (true) {
+			System.out.println("달을 입력하세요.");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12) {
+				continue;
+			}
+
+			cal.printCalender(2021, month);
+		}
+
+		System.out.println("fin~");
+		scanner.close();
+	}
+
+
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
+
+	// 숫자로 입력받아 해당하는달의 최대일수를 출력하는 프로그램
+
+}
